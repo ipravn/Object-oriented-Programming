75b648e5de448e9c0b497a25e856a031f8763bb9
Praveen Kumar <46933956+ipravn@users.noreply.github.com>
2022-08-30 22:50:00 +0530

    Update CopyOfRange in C#.cs

--- CopyOfRange in C#.cs
+++ CopyOfRange in C#.cs
@@ -4,3 +4,37 @@ static int[] copyOfRange (int[] src, int start, int end) {
             Array.Copy(src, start, dest, 0, len);
             return dest;
         }
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 78, 9, 14, 51, 7, 99, 11, 47, 87 };
+            int mid = arr.Length / 2;
+            
+            int[] left = copyOfRange(arr, 0, mid);
+            int[] right = copyOfRange(arr, mid, arr.Length);
+            
+            Console.WriteLine(String.Join( ",",left));
+            Console.WriteLine(String.Join( ",",right));
+            
+        }
+        
+        
+        static int[] copyOfRange (int[] src, int start, int end) {
+            int len = end - start;
+            int[] dest = new int[len];
+            Array.Copy(src, start, dest, 0, len);
+            return dest;
+        }
+
+       
+        
+    }*/
+            
+ //Output:
+ //78,9,14,51
+ //7,99,11,47,87
